1c08ac25db1515dc8ef22b48b3ecf42407ed333b
LINKY1116 <98253730+LINKY1116@users.noreply.github.com>
2022-01-23 17:42:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 
 <head>
-    <title>吃掉小鸡哥</title>
-    <meta itemprop="name" content="吃掉小鸡哥" />
+    <title>吃掉鸡哥</title>
+    <meta itemprop="name" content="吃掉鸡哥" />
     <meta itemprop="description" content="新概念音游" />
     <meta itemprop="image" content="https://www.thac.cc/kano/res/logo.jpg" />
     <meta charset="utf-8" />
