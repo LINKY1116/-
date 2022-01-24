7b564175ee48124f335d6149c1bca4c28523e475
LINKY1116 <98253730+LINKY1116@users.noreply.github.com>
2022-01-24 22:59:59 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -337,9 +337,9 @@ function backBtn() {
 function shareText(score) {
     var date2 = new Date();
     deviation_time = (date2.getTime() - _date1.getTime())
- if (deviation_time > 29000) {
+    if (deviation_time > 29000) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";    
-}
+    }
     SubmitResults();
     if (score <= 49) return '鸡都还没熟呢..';
     if (score <= 99) return '鸡好吃吗?';
